e4d9a722a242d695b098465b3ef6ba288bce8e3b
Ahmed_1901 <ahmed.jawad01@northsouth.edu>
2023-03-07 16:35:41 +0600

    added comment features

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let parent = document.getElementsByClassName("blogpost-container")[0]; 
+let parent = document.getElementsByClassName("blogpost-container")[0];
 const blog_div = document.getElementsByClassName("blogpost")[0];
 const tempUser = JSON.parse(window.sessionStorage.getItem("currentUser"));
 const inputField = document.getElementById("input");
@@ -7,6 +7,7 @@ const edit_cancel = document.getElementById("edit_blog_cancel_btn");
 const edit_save = document.getElementById("edit_blog_save_btn");
 const modal = document.getElementById("myModal");
 const modal_input = document.getElementById("edit_blog_input_textarea");
+const commentMapElement = document.querySelector("#template_comment");
 // const like_btn = document.getElementById
 var trackBlogId = 0;
 
@@ -25,34 +26,36 @@ async function createPost()
         const blog_list = await getAllBlogsFromEndPoint();
         const mostRecentBlog = getMostRecentBlogId(user.username, blog_list);
         const blog_uuid = mostRecentBlog.uuid;
-        console.log(blog_uuid);
-        addPostToDOM(content, user.username, blog_uuid);
+        // console.log(blog_uuid);
+        addPostToDOM(content, user.username, new Date(), blog_uuid);
     }
 }
-function addPostToDOM(blog_content, author, blog_uuid, likes_list)
+function addPostToDOM(blog_content, author, date, blog_uuid, likes_list, comments_list)
 {
     const dom_blog = blog_div.cloneNode(true);
     dom_blog.querySelector(".username").textContent = `${author}`;
     dom_blog.querySelector(".blogpost_text").innerText = `${blog_content}`;
     dom_blog.id = `${blog_uuid}`;
-    //dom_blog.querySelector(".blogpost_time").innerText = `${blog.date.toLocaleDateString() + " " + blog.date.toLocaleTimeString()}`;
+    dom_blog.querySelector(".blogpost_time").innerText = `${date.toDateString()}`;
     const edit = dom_blog.querySelector(".edit_btn");
     const del = dom_blog.querySelector(".delete_btn");
     let same_user_liked_check = undefined;
-    let count = 0
+    let comment_count = dom_blog.querySelector(".comment-count");
     let like_count = dom_blog.querySelector(".like-count");
-    console.log("like-list: ", likes_list);
     if(likes_list!==undefined)
     {
-        console.log("working")
+        // console.log("working")
         same_user_liked_check = likes_list.find(element => element.user === user.username)
-        //same_user_liked_check = likes_list.find(element => {element.user === user.username})
         like_count.getElementsByTagName("span")[1].textContent = likes_list.length;
     }
-    console.log("same user liked?: ", same_user_liked_check)
+    if(comments_list!==undefined)
+    {
+        comment_count.getElementsByTagName("span")[1].textContent = comments_list.length;
+    }
+    // console.log("same user liked?: ", same_user_liked_check)
     console.log(like_count.getElementsByTagName("span")[1].textContent)
     const like_btn = dom_blog.querySelector(".like_btn");
-    console.log(like_btn);
+    // console.log(like_btn);
     if(same_user_liked_check!==undefined)
     {
         like_btn.textContent = "Liked!";
@@ -73,10 +76,10 @@ async function render()
         parent.removeChild(first_ele)
 
     let list_of_blogs = await getAllBlogsFromEndPoint();
-    console.log(list_of_blogs)
+    // console.log(list_of_blogs)
     for(let i=0;i<list_of_blogs.length;i++)
     {
-        addPostToDOM(list_of_blogs[i].content, list_of_blogs[i].author, list_of_blogs[i].uuid, list_of_blogs[i].likes);
+        addPostToDOM(list_of_blogs[i].content, list_of_blogs[i].author, new Date(list_of_blogs[i].created_at), list_of_blogs[i].uuid, list_of_blogs[i].likes, list_of_blogs[i].comments);
     }
 }
 document.getElementById("logout").addEventListener("click", ()=>{;
@@ -106,6 +109,36 @@ async function getAllBlogsFromEndPoint()
        .then(response => response.json())
        .catch(e => console.log(e.message));
     return response;
+}
+
+function addSingleCommentToDOMElement(comment_message, comment_time, commenter, blog_id)
+{
+    let currentBlogElement = document.getElementById(blog_id);
+    const newCommentElement = commentMapElement.cloneNode(true);
+    newCommentElement.id = blog_id;
+    newCommentElement.querySelector(".comment_text").textContent = comment_message
+    newCommentElement.querySelector(".comment_time").textContent = new Date(comment_time).toDateString();
+    newCommentElement.getElementsByTagName("h4")[0].textContent = commenter;
+    newCommentElement.style.display = 'block';
+    const thisCommentList = currentBlogElement.querySelector(".comment-list");
+    thisCommentList.insertBefore(newCommentElement, thisCommentList.firstChild);
+}
+async function addCommentsToDOMElement(blog_id)
+{
+    const blog = await getBlogById(blog_id);
+    // console.log(blog);
+    let len = 0;
+    if(blog.comments === undefined || blog.comments.length === 0)
+        len = 0;
+    else
+        len = blog.comments.length;
+    // console.log("Length of comments", len);
+    let currentBlogElement = document.getElementById(blog_id);
+    currentBlogElement.querySelector(".comment-input").style.display = 'flex';
+    for(let i=0;i<len;i++)
+    {
+        addSingleCommentToDOMElement(blog.comments[i].message, blog.comments[i].created_at, blog.comments[i].user, blog_id);
+    }
 }
 
 async function postToEndPoint(blog_content, blog_author)
@@ -153,7 +186,7 @@ async function deleteBlogById(blog_id)
 async function updateBlogContenttoEndPoint(blog_content, blog_id)
 {
     const data = {content:blog_content}
-    console.log("Data is: ", data);
+    // console.log("Data is: ", data);
     const proxyUrl = `http://localhost:4000/blog/${blog_id}`;
     const success = await fetch(proxyUrl,{
         method: 'PATCH',
@@ -176,8 +209,6 @@ async function updateBlogContenttoEndPoint(blog_content, blog_id)
 async function addLike(blog_id, liked_by)
 {
     const data = {user:liked_by};
-    console.log(data);
-    console.log(blog_id)
     const proxyUrl = `http://localhost:4000/blog/like/${blog_id}`;
     const success = await fetch(proxyUrl, {
         method: 'POST',
@@ -192,6 +223,27 @@ async function addLike(blog_id, liked_by)
     })
     return success;
 
+}
+async function addComment(blog_id, comment_message, commented_by)
+{
+    console.log("blog_id is: ", blog_id);
+    const data = {message:comment_message, comment_message:commented_by}
+    const proxyUrl = `http://localhost:4000/blog/commnet/${blog_id}`;
+    const success = await fetch(proxyUrl, {
+        method: "POST",
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if(response.ok)
+            {
+                console.log("successfully added comment");
+                return true;
+            }
+        else
+            throw new Error("Comment POST request failed");
+    })
+    return success;
 }
 async function getBlogById(id)
 {
@@ -210,16 +262,13 @@ function getMostRecentBlogId(author, bloglist)
     const filteredList = bloglist.filter(blog => blog.author===author);
     return filteredList[filteredList.length - 1];
 }
-
 edit_cancel.addEventListener("click", ()=>{
     modal.style.display = "none";
 })
 
 edit_save.addEventListener("click", ()=>{
     const new_text = modal_input.value;
-    console.log("New input: ", new_text)
     const blog_id = trackBlogId;
-    console.log(blog_id);
     let success = updateBlogContenttoEndPoint(new_text, blog_id);
     if(success)
     {
@@ -273,6 +322,23 @@ function handle_blog_events(e)
         //     e.target.style.background = "none";
         //     e.target.style.color = "blueviolet";
         // }
+    }
+    else if(e.target.classList.contains("comment_btn"))
+    {
+        trackBlogId = e.target.parentNode.parentNode.parentNode.parentNode.id;
+        // console.log("clicked comment")
+        addCommentsToDOMElement(trackBlogId);
+    }
+    else if(e.target.classList.contains("comment_submit_btn"))
+    {   trackBlogId = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.id;
+        // console.log(trackBlogId);
+        // console.log("clicked comment submit");
+        const input = e.target.parentNode.querySelector(".comment");
+        let comment = input.value;
+        input.value = "";
+        const success = addComment(trackBlogId, comment, user.username)
+        .then(addCommentsToDOMElement(trackBlogId))
+        .catch(e => console.log("error: ", e))
     }
     //to be added for likes/comments
 }
