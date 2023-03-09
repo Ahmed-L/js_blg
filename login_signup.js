66014da8b7854aa3da9fb1c7cd7d5f834e6afdcc
Ahmed_1901 <ahmed.jawad01@northsouth.edu>
2023-03-09 17:41:16 +0600

    minor changes in formatting

--- login_signup.js
+++ login_signup.js
@@ -4,27 +4,27 @@ var userList = []
 if(window.localStorage.getItem("userList"))
     userList = JSON.parse(window.localStorage.getItem("userList"));
 
-const feedbackMsg = document.getElementById("feedback");
-const isLoginPage = document.getElementsByTagName("h2")[0].textContent === "Log in"?true:false;
-const buttonId = isLoginPage?"login_btn":"signup_btn";
-const authOrReg = isLoginPage?auth:reg;
-const btn = document.getElementById(buttonId);
+let feedbackMsg = document.getElementById("feedback");
+let isLoginPage = document.getElementsByTagName("h2")[0].textContent === "Log in"?true:false;
+let buttonId = isLoginPage?"login_btn":"signup_btn";
+let authOrReg = isLoginPage?auth:reg;
+let btn = document.getElementById(buttonId);
 
 function auth()
 {
-    const user = document.getElementById("user").value.toString();
-    const pass = document.getElementById("pass").value.toString();
-    const userFoundOrFalse = checkUserList(user, userList);
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+    let userFoundOrFalse = checkUserList(user, userList);
     if(!userFoundOrFalse)
     {
-        console.log(user, pass);
+        // console.log(user, pass);
         feedbackMsg.textContent = "No registered user with this username."
         return;
     }
     if(userFoundOrFalse.password === pass)
     {
         window.sessionStorage.setItem("currentUser", JSON.stringify(userFoundOrFalse));
-        window.location.href = "index.html";
+        window.location.href = "./index.html";
     }
     else
         feedbackMsg.textContent = "Incorrect password.";
@@ -34,8 +34,8 @@ function reg()
 {
     const user_inputField = document.getElementById("user");
     const pass_inputField = document.getElementById("pass");
-    const user = user_inputField.value.trim()
-    const pass = pass_inputField.value.trim()
+    let user = user_inputField.value.trim()
+    let pass = pass_inputField.value.trim()
     if(user===""||pass==="")
     {
         feedbackMsg.textContent = "Input fields cannot be blank";
@@ -49,8 +49,6 @@ function reg()
                 return;
             }
         const new_user = new User(user, "random url", pass);
-        console.log(new_user.usename + " obj: username");
-        console.log(new_user.pass + " obj: pass");
         userList.push(new_user);
         const isUpdateSuccessful = User.updateUserStorage(userList);
         if(!isUpdateSuccessful)
@@ -65,10 +63,7 @@ function checkUserList(username, userList)
 {
     if(userList.length>0)
     {
-        console.log(userList)
-        console.log(username)
         const userFoundOrFalse = userList.find(element => element.username===username);
-        console.log(userFoundOrFalse);
         if(userFoundOrFalse!==undefined)
             return userFoundOrFalse;
         else
